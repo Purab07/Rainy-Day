e066ca8ee5140cafe0648b6f8c44e8a988fd8354
Purab07 <73265862+Purab07@users.noreply.github.com>
2022-07-14 11:58:05 -0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,7 +3,7 @@ const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Constraint = Matter.Constraint;
 var maxDrops = 100;
-
+drops = []
 function preload(){
     
 }
@@ -13,19 +13,21 @@ function setup(){
     
     engine = Engine.create();
     world = engine.world;
-
-     drops = new Drops(100,100,2,2);
+    
+    if(frameCount % 150 === 0){
+        for(var i=0; i<maxDrops; i++){
+            drops.push(new Drops(random(0,400), random(0,400),5));
+        }
+    }
 }
 
 function draw(){
     background("black");
     drops.display();
-    for(var i=0; i<maxDrops;i++){
-        drops.push(new Drops(random(0,400),random(0,400)))
-        if(this.rain.position.y > height){
-            Matter.body.setPosition(this.rain,{x:random(0,400),y:random(0,400)})
-        }
-    }
     
+    for(var i=0; i<maxDrops;i++){
+        drops[i].display();
+        drops[i].updateY();
+    }
 }   
 
